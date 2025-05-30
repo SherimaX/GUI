e7417f6a522493a20b2a3966ebafc23094d6a4c6
SherimaX <46977789+SherimaX@users.noreply.github.com>
2025-05-30 10:47:34 -0400

    Disable Dash debug UI

--- app.py
+++ app.py
@@ -822,7 +822,7 @@ if __name__ == "__main__":
     dash_app.run(
         host="127.0.0.1",
         port=8050,
-        debug=True,
+        debug=False,
         use_reloader=False,
         threaded=True,
         )
